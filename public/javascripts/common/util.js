0e04d167397b5f67b47bfde3fd1b3952b5dff194
alpha <meringk@alpha.co.kr>
2017-01-20 13:35:55 +0900

    CSS

--- public/javascripts/common/util.js
+++ public/javascripts/common/util.js
@@ -1,6 +1,46 @@
-(function($) {
+(function ($) {
 
-    
-	
+    var $window = $(window),
+        $body = $('body');
+
+
+    // Disable animations/transitions until the page has loaded.
+    $body.addClass('is-loading');
+
+    $window.on('load', function () {
+        window.setTimeout(function () {
+            
+            $body.removeClass('is-loading');
+            //코드를 다 부른 뒤(window.on.load)에 처리해야한다. (element들이 전부 load되고)
+            var $toggle = $('.toggle'),
+                $sidebarSection = $('#sidebarSection');
+
+            // sidebar 나타내는 toggle 버튼 flag
+            $toggle[0].onclick = function () {
+                $sidebarSection.toggleClass("inactive");
+                $body.toggleClass("side-on");
+            }
+
+            // click 이벤트 외의 별도의 브라우저 행동을 막는다. (bubble UP막기)
+            $sidebarSection.on('click touchend touchstart touchmove', function (event) {
+                event.stopPropagation();
+            });
+            $toggle.on('click touchend touchstart touchmove', function (event) {
+                event.stopPropagation();
+            });
+
+            // 바디 터치시 사이드바 숨김
+            $body.on('click touchend', function (event) {
+                $sidebarSection.addClass('inactive');
+            });
+
+        }, 100);
+
+
+
+    });
+
+
+
 
 })(jQuery);
--- public/javascripts/sidebar.js
+++ public/javascripts/sidebar.js
@@ -1,28 +1,22 @@
+var $menu = $('#menu'),
+	$menu_openers = $menu.children('ul').find('.opener');
 
-	// Menu.
-	var $menu = $('#menu'),
-		$menu_openers = $menu.children('ul').find('.opener');
-		console.log("=------------------------")
-		console.log($menu_openers);
-	// Openers.
-		$menu_openers.each(function() {
+// Openers.
+$menu_openers.each(function () {
 
-			var $this = $(this);
+	var $this = $(this);
 
-			console.log($this);
+	console.log($this);
 
-			$this.on('click', function(event) {
+	$this.on('click', function (event) {
 
-				// Prevent default.
-					event.preventDefault();
+		// Prevent default.
+		event.preventDefault();
 
-				// Toggle.
-					$menu_openers.not($this).removeClass('active');
-					$this.toggleClass('active');
+		// Toggle.
+		$menu_openers.not($this).removeClass('active');
+		$this.toggleClass('active');
 
-				// Trigger resize (sidebar lock).
-					// $window.triggerHandler('resize.sidebar-lock');
+	});
 
-			});
-
-		});
+});
