6a0cd43ef29e49edf1821937f3b88edc364d8309
alpha <meringk@alpha.co.kr>
2017-02-14 17:23:30 +0900

    no message

--- app/main/main_controller.js
+++ app/main/main_controller.js
@@ -8,40 +8,38 @@ var g_idx = 0;
 
 //네이버로그인
 
-var client_id = 'E9pMRy_OGTUOp3FORXZ3';
-var client_secret = 'D8zx4ZgXYr';
-var state = "RANDOM_STATE";
-var redirectURI = encodeURI("http://meringk.com/naver_login_success_page.html");
-var api_url = "";
+// var client_id = 'E9pMRy_OGTUOp3FORXZ3';
+// var client_secret = 'D8zx4ZgXYr';
+// var state = "RANDOM_STATE";
+// var redirectURI = encodeURI("http://meringk.com/naver_login_success_page.html");
+
+
+
+
+// var api_url  = encodeURI("http://meringk.com/naver_login_success_page.html");
 
 
 
 router.get('/naver_login_success', function(req, res){
-    console.log(req.query)
-    code = req.query.code;
-    state = req.query.state;
-    api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
-     + client_id + '&client_secret=' + client_secret + '&redirect_uri=' + redirectURI + '&code=' + code + '&state=' + state;
+
+    res.redirect('http://meringk.com/naver_login_success_page.html');
 
 
-     console.log("api_urlapi_urlapi_urlapi_url");
-     console.log(api_url);
-
-
-    var request = require('request');
-    var options = {
-        url: api_url,
-        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
-     };
-    request.get(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-        res.end(body);
-      } else {
-        res.status(response.statusCode).end();
-        console.log('error = ' + response.statusCode);
-      }
-    });
+    // console.log(req.query)
+    // var request = require('request');
+    // var options = {
+    //     url: api_url,
+    //     headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+    //  };
+    // request.get(options, function (error, response, body) {
+    //   if (!error && response.statusCode == 200) {
+    //     res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+    //     res.end(body);
+    //   } else {
+    //     res.status(response.statusCode).end();
+    //     console.log('error = ' + response.statusCode);
+    //   }
+    // });
 });
 
 
